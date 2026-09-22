510754ea2e4243835aaf34c0a9779139fc0b3be6
Shirin Rahman <srahman25-ops@users.noreply.github.com>
2026-09-22 15:50:06 +0200

    Redesign Raval bars passport interface

---
+++ bares.js
@@ -0,0 +1,271 @@
+const bares = [
+    {
+        id: 1,
+        nombre: "Bar Atlanta",
+        visitado: false
+    },
+    {
+        id: 2,
+        nombre: "Bar El Toro",
+        visitado: false
+    },
+    {
+        id: 3,
+        nombre: "Fat Cat",
+        visitado: false
+    },
+    {
+        id: 4,
+        nombre: "La Prudència",
+        visitado: false
+    },
+    {
+        id: 5,
+        nombre: "Las Fernández",
+        visitado: false
+    },
+    {
+        id: 6,
+        nombre: "Nun Cultural Café",
+        visitado: false
+    },
+    {
+        id: 7,
+        nombre: "Chula Vista",
+        visitado: false
+    },
+    {
+        id: 8,
+        nombre: "El Magraner Boig",
+        visitado: false
+    },
+    {
+        id: 9,
+        nombre: "El Cafetí",
+        visitado: false
+    },
+    {
+        id: 10,
+        nombre: "La Ravala",
+        visitado: false
+    },
+    {
+        id: 11,
+        nombre: "La Monroe",
+        visitado: false
+    },
+    {
+        id: 12,
+        nombre: "Arume",
+        visitado: false
+    },
+    {
+        id: 13,
+        nombre: "La Morera",
+        visitado: false
+    },
+    {
+        id: 14,
+        nombre: "Bar Mendizábal",
+        visitado: false
+    },
+    {
+        id: 15,
+        nombre: "Dr. Beer & Mr. Fried",
+        visitado: false
+    },
+    {
+        id: 16,
+        nombre: "Biocenter",
+        visitado: false
+    },
+    {
+        id: 17,
+        nombre: "Bar Costa",
+        visitado: false
+    },
+    {
+        id: 18,
+        nombre: "Clemen's Boqueria",
+        visitado: false
+    },
+    {
+        id: 19,
+        nombre: "L'Universal",
+        visitado: false
+    }
+]
+
+// Acceso y saludo
+const formularioAcceso = document.getElementById('formularioAcceso')
+
+function obtenerNombreUsuario() {
+    const nombre = document.getElementById('nombre').value
+    return nombre
+}
+
+function nombreValido(nombre) {
+    if (nombre.trim() === '') {
+        return false
+    } else {
+        return true
+    }
+}
+
+function mostrarSaludo(nombre) {
+    const saludo = document.getElementById('saludo')
+    saludo.textContent = `Hola ${nombre}`
+}
+
+formularioAcceso.addEventListener('submit', function (evento) {
+    evento.preventDefault()
+    const nombre = obtenerNombreUsuario()
+    if (nombreValido(nombre) === false) {
+        document.getElementById('mensajeAcceso').textContent = '¡Introduce tu nombre!'
+        return
+    }
+    document.getElementById('mensajeAcceso').textContent = ''
+    mostrarSaludo(nombre)
+
+    document.getElementById('acceso').style.display = 'none'
+    document.getElementById('pasaporte').style.display = 'block'
+
+    // mostrarBares() 
+    actualizarAplicacion()
+})
+
+// Gestionamos los bares
+
+function buscarBarPorId(idBar) {
+    for (let i = 0; i < bares.length; i++) {
+        if (bares[i].id === idBar) {
+            return bares[i]
+        }
+    }
+    return null
+}
+
+function sellarBar(idBar) {
+    const bar = buscarBarPorId(idBar)
+
+    if (bar === null) {
+        return 'No se ha encontrado el bar'
+    }
+
+    if (bar.visitado === true) {
+        return 'Este bar ya estaba sellado'
+    }
+
+    bar.visitado = true
+    return `Has sellado tu visita en ${bar.nombre}`
+}
+
+// función para mostrar los bares
+function mostrarBares() {
+    const listaBares = document.getElementById('listaBares')
+    listaBares.innerHTML = ''
+    for (let i = 0; i < bares.length; i++) {
+        const tarjeta = document.createElement('article')
+        const numero = document.createElement('p')
+        const nombre = document.createElement('h3')
+        const estado = document.createElement('p')
+        const boton = document.createElement('button')
+
+        tarjeta.classList.add('bar') 
+        numero.classList.add('numero-bar') 
+        estado.classList.add('estado') 
+
+        numero.textContent = `Parada ${bares[i].id}`
+        nombre.textContent = bares[i].nombre
+
+        if (bares[i].visitado === true) {
+            estado.textContent = 'Visita sellada'
+            boton.textContent = 'Ya sellado'
+            boton.disabled = true
+            tarjeta.classList.add('bar-visitado')
+        } else {
+            estado.textContent = ' Pendiente'
+            boton.textContent = 'Sellar visita'
+        }
+
+        boton.addEventListener('click', function () {
+            const mensaje = sellarBar(bares[i].id)
+            document.getElementById('mensajeSello').textContent = mensaje
+            actualizarAplicacion()
+        })
+
+        tarjeta.appendChild(numero)
+        tarjeta.appendChild(nombre)
+        tarjeta.appendChild(estado)
+        tarjeta.appendChild(boton)
+
+        listaBares.appendChild(tarjeta)
+    }
+}
+
+// Gestión del estado
+
+function contarBaresVisitados(){
+    let contador = 0
+
+    for(let i = 0; i < bares.length; i++){
+        if(bares[i].visitado === true){
+            contador++
+        }
+    }
+    return contador
+}
+
+
+
+function calcularPorcentaje(visitados, total){
+    const porcentaje = (visitados/total)*100
+    return Math.round(porcentaje)
+}
+
+function crearMensajeProgreso(visitados, total){
+    const porcentaje = calcularPorcentaje(visitados, total)
+    const mensaje = `Has visitado ${visitados} bares, de un total de ${total} bares. Tu progreso es de un ${porcentaje}%`
+    return mensaje
+}
+
+function mostrarProgreso(){
+    const visitados = contarBaresVisitados()
+    const total = bares.length
+    const mensaje = crearMensajeProgreso(visitados, total)
+    document.getElementById('progreso').textContent = mensaje
+}
+
+function comprobarPasaporteCompleto(){
+    const visitados = contarBaresVisitados()
+    const total = bares.length
+
+    if(visitados === total){
+        return '¡Pasaporte completo!'
+    }
+
+    const pendientes = total - visitados
+    return `Te faltan ${pendientes} bares por visitar.`
+}
+
+function mostrarEstadoPasaporte(){
+    const mensaje = comprobarPasaporteCompleto()
+    document.getElementById('estadoPasaporte').textContent = mensaje
+}
+
+function actualizarBarraProgreso(){
+    const visitados = contarBaresVisitados()
+    const porcentaje = calcularPorcentaje(visitados, bares.length)
+    const barraProgreso = document.querySelector('.barraProgreso')
+    barraProgreso.style.width = `${porcentaje}%`
+    barraProgreso.parentElement.setAttribute('aria-valuenow', porcentaje)
+}
+
+
+
+function actualizarAplicacion(){
+    mostrarBares()
+    mostrarProgreso()
+    mostrarEstadoPasaporte()
+    actualizarBarraProgreso()
+}
